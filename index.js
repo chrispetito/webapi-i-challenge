7b4418e9892a4c095f08ef79e8e6c94000995dac
chrispetito <christopherpetito@gmail.com>
2019-05-27 14:28:09 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello WEB19');
+})
+
+server.get('/now', (req, res) => {
+    const now = new Date().toISOString();
+    res.send(now);
+})
+
+server.listen(5000, () => {
+    console.log('API up and running on port 5k')
+})
