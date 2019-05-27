be04b82d6c24af0104ae579f701d07f7d34041bd
chrispetito <christopherpetito@gmail.com>
2019-05-27 14:48:27 -0700

    add get endpoints

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 // implement your API here
-const express = require('express');
-const db = require('./data/db');
+const express = require("express");
+const db = require("./data/db");
 
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send('Hello WEB19');
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("Hello WEB19");
+});
+
+server.get("/api/users", (req, res) => {
+  db.find().then(users => res.json({ users }));
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id).then(user => {
+        if (user.length === 0) {
+            return res.status(404).json({ message: 'The user with the specified ID does not exist.'})
+        }
+        res.json({ user })
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'The user information could not be retrieved.'})
+    })
 })
 
-server.get('/now', (req, res) => {
-    const now = new Date().toISOString();
-    res.send(now);
-})
+server.get("/now", (req, res) => {
+  const now = new Date().toISOString();
+  res.send(now);
+});
 
 server.listen(5000, () => {
-    console.log('API up and running on port 5k')
-})
+  console.log("API up and running on port 5k");
+});
